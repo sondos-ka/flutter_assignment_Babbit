38d746afbb4f5961f3a8c953681302ffaf769590
sondos-ka <68806414+sondos-ka@users.noreply.github.com>
2021-07-05 12:05:41 +0300

    Add files via upload

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,28 +1,55 @@
 
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutterassignment/base/Product.dart';
 import 'package:flutterassignment/base/const.dart';
-import 'package:flutterassignment/base/product_list.dart';
 import 'package:flutterassignment/base/rootWidgets.dart';
 import 'package:flutterassignment/base/strings.dart';
-import 'package:flutterassignment/services/dataBase.dart';
-import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+String price="";
+var discount;
+var Size;
+class ProductsRepository {
+
+  Future<List<dynamic>> getProducts() async {
+    final response = await http.get("https://f8520130-7c98-4e64-8490-9e1a9c377eb4.mock.pstmn.io/api/products/272?env=test");
+    var data = json.decode(response.body);
+    price=data["unit_price"];
+    discount=data["unit_discount_price"];
+    Size=data["product_size"];
+
+
+
+    List<dynamic> productImages = data["product_images"];
+    print(productImages[0]["media_url"]);
+    return productImages;
+  }
+
+}
+
+
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List cats= List<Cat>();
+class _HomeScreenState extends State<HomeScreen>  {
+  Future<Product> futureProduct;
+
+List cats= List<Cat>();
   int curPage=3;
   int selectedListItem1=0;
   int selectedListItem2=0;
   int selectedListItem3=0;
+
+
+
   @override
   void initState() {
    cats..add(Cat(id: 1, name: sCat1, image: sImgCat1
@@ -52,6 +79,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ))
 
       ;
+
         super.initState();
 
 
@@ -62,162 +90,263 @@ class _HomeScreenState extends State<HomeScreen> {
     double width=MediaQuery.of(context).size.width;
 
     return
-      StreamProvider<List<Product>>.value(
-        value: dataBase().products,
-
-        child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        backgroundColor: Colors.white,
+      Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
 
 
-          bottomNavigationBar: BottomNavigationBar(
-            selectedItemColor:cRedLine,
-            currentIndex : curPage,
-              onTap: (int index) {
-          setState(() {
-          this.curPage = index;
-          },);},//
-              // this will be set when a new tab is tapped
-            items: [
-              BottomNavigationBarItem(
-                  icon:  curPage== 0?  SvgPicture.asset('assets/Icons/User off-1.svg'): SvgPicture.asset('assets/Icons/User off.svg'),
-                  label:sUser
-              ), BottomNavigationBarItem(
-                  icon: curPage== 1?SvgPicture.asset('assets/Icons/categories off-1.svg'):SvgPicture.asset('assets/Icons/categories off.svg'),
-                  label:sCategories
-              ),BottomNavigationBarItem(
-                icon: curPage==2 ? SvgPicture.asset('assets/Icons/chat off-1.svg'):SvgPicture.asset('assets/Icons/chat off.svg'),
-                label: sChat,
-              ),BottomNavigationBarItem(
-                icon: curPage==3 ? SvgPicture.asset('assets/Icons/Home on-1.svg'):SvgPicture.asset('assets/Icons/Home on.svg'),
-                label: sHome,
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:cRedLine,
+          currentIndex : curPage,
+            onTap: (int index) {
+        setState(() {
+        this.curPage = index;
+        },);},//
+            // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+                icon:  curPage== 0?  SvgPicture.asset('assets/Icons/User off-1.svg'): SvgPicture.asset('assets/Icons/User off.svg'),
+                label:sUser
+            ), BottomNavigationBarItem(
+                icon: curPage== 1?SvgPicture.asset('assets/Icons/categories off-1.svg'):SvgPicture.asset('assets/Icons/categories off.svg'),
+                label:sCategories
+            ),BottomNavigationBarItem(
+              icon: curPage==2 ? SvgPicture.asset('assets/Icons/chat off-1.svg'):SvgPicture.asset('assets/Icons/chat off.svg'),
+              label: sChat,
+            ),BottomNavigationBarItem(
+              icon: curPage==3 ? SvgPicture.asset('assets/Icons/Home on-1.svg'):SvgPicture.asset('assets/Icons/Home on.svg'),
+              label: sHome,
 
 
+            ),
+          ],
+        ),
+
+      body: SingleChildScrollView(
+        child: SafeArea(
+
+          child: Column(
+            children: [
+
+              logoBlock(MediaQuery.of(context).size.width),
+
+            Text(sCategories,style:categoryLableStyle),
+
+            Container(
+              color: cRedLine,height:2,width: 85,)
+
+         , Container(
+                padding: EdgeInsets.all(10),
+          height: MediaQuery.of(context).orientation== Orientation.landscape?MediaQuery.of(context).size.height*3-30:MediaQuery.of(context).size.height-70,
+                   child: //for stop scroll in gridView
+                IgnorePointer(
+                  child: GridView.builder(
+
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio:1.85
+                      ),
+                      itemCount: cats.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                            child: categoriesBlock('assets/BabitCategories/'+cats[index].image+'.png',cats[index].name,width/2-10));
+                      }
+                  ),
+                )
+         )
+             ,
+              Container(
+                padding: EdgeInsets.fromLTRB(0,20,15,0),
+                height: 50,
+                child: ListView.builder(
+                  reverse: true,
+                 // shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: cats.length,
+                  itemBuilder: (BuildContext context, int index){
+                     return GestureDetector(
+                       onTap: (){setState(() {
+                         selectedListItem1=index;
+                       });},
+                       child: Container(
+                         padding: EdgeInsets.only(left: 40),
+                           child: Text(catListView[index], style:selectedListItem1==index?categoryListStyleSelected :categoryListStyle,)),
+                     );
+                  }
+                ),
               ),
-            ],
-          ),
+              FutureBuilder<List<dynamic>>(
+                future: ProductsRepository().getProducts(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
 
-        body: SingleChildScrollView(
-          child: SafeArea(
+                  } else if (snapshot.hasData) {
+                    return
+                      Container(
+                        height:MediaQuery.of(context).orientation==Orientation.portrait? 500:850,
+                        child: IgnorePointer(
+                          child: Container(
+                            padding: EdgeInsets.all( 10),
+                            child: GridView.builder(
+                                gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  childAspectRatio:MediaQuery.of(context).orientation==Orientation.portrait? width/750: width/1300,
+                                ),
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return productShow(width / 3 - 10, snapshot.data[index]["media_url"],
+                                      price );
+                                }
+                            ),
+                          ),
+                        ),
+                      );
+                    /*Text(
+                      snapshot.data[0]["media_url"]
 
-            child: Column(
-              children: [
-
-                logoBlock(MediaQuery.of(context).size.width),
-
-              Text(sCategories,style:categoryLableStyle),
+                    );*/
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
 
               Container(
-                color: cRedLine,height:2,width: 85,)
+                  padding: EdgeInsets.all(20),
 
-           , Container(
-                  padding: EdgeInsets.all(10),
-            height: MediaQuery.of(context).orientation== Orientation.landscape?MediaQuery.of(context).size.height*3-30:MediaQuery.of(context).size.height-70,
-                     child: //for stop scroll in gridView
-                  IgnorePointer(
-                    child: GridView.builder(
+                  child: watchMoreButton((){},width)),
 
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio:1.85
-                        ),
-                        itemCount: cats.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                              child: categoriesBlock('assets/BabitCategories/'+cats[index].image+'.png',cats[index].name,width/2-10));
-                        }
-                    ),
-                  )
-           )
-               ,
-                Container(
-                  padding: EdgeInsets.fromLTRB(0,20,15,0),
-                  height: 50,
-                  child: ListView.builder(
+              Container(
+
+                  child: imageText("assets/Banners/special-sizes.png",sBigSizes,width,imageTextStyle)),
+              Container(
+                padding: EdgeInsets.fromLTRB(0,20,15,0),
+                height: 50,
+                child: ListView.builder(
                     reverse: true,
-                   // shrinkWrap: true,
+                    // shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
                     itemCount: cats.length,
                     itemBuilder: (BuildContext context, int index){
-                       return GestureDetector(
-                         onTap: (){setState(() {
-                           selectedListItem1=index;
-                         });},
-                         child: Container(
-                           padding: EdgeInsets.only(left: 40),
-                             child: Text(catListView[index], style:selectedListItem1==index?categoryListStyleSelected :categoryListStyle,)),
-                       );
+                      return GestureDetector(
+                        onTap: (){setState(() {
+                          selectedListItem2=index;
+                        });},
+                        child: Container(
+                            padding: EdgeInsets.only(left: 40),
+                            child: Text(catListView[index], style: selectedListItem2==index?categoryListStyleSelected :categoryListStyle,)),
+                      );
                     }
-                  ),
                 ),
-                Container(
-                   height:MediaQuery.of(context).orientation==Orientation.portrait? 500: 850,
-                    child: ProductList()),
-                Container(
-                    padding: EdgeInsets.all(20),
+              ),
+              FutureBuilder<List<dynamic>>(
+                future: ProductsRepository().getProducts(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
 
-                    child: watchMoreButton((){},width)),
-
-                Container(
-
-                    child: imageText("assets/Banners/special-sizes.png",sBigSizes,width,imageTextStyle)),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0,20,15,0),
-                  height: 50,
-                  child: ListView.builder(
-                      reverse: true,
-                      // shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: cats.length,
-                      itemBuilder: (BuildContext context, int index){
-                        return GestureDetector(
-                          onTap: (){setState(() {
-                            selectedListItem2=index;
-                          });},
+                  } else if (snapshot.hasData) {
+                    return
+                      Container(
+                        height:MediaQuery.of(context).orientation==Orientation.portrait? 500:850,
+                        child: IgnorePointer(
                           child: Container(
-                              padding: EdgeInsets.only(left: 40),
-                              child: Text(catListView[index], style: selectedListItem2==index?categoryListStyleSelected :categoryListStyle,)),
-                        );
-                      }
-                  ),
+                            padding: EdgeInsets.all( 10),
+                            child: GridView.builder(
+                                gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  childAspectRatio:MediaQuery.of(context).orientation==Orientation.portrait? width/750: width/1300,
+                                ),
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return productShow(width / 3 - 10, snapshot.data[index]["media_url"],
+                                      price );
+                                }
+                            ),
+                          ),
+                        ),
+                      );
+                    /*Text(
+                      snapshot.data[0]["media_url"]
+
+                    );*/
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+              Container(
+                  padding: EdgeInsets.all(20),
+
+                  child: watchMoreButton((){},width)),
+              Container(
+
+                  child: imageText("assets/Banners/sale-banner.png",sDiscount,width,imageTextStyleWhite)),
+              Container(
+                padding: EdgeInsets.fromLTRB(0,20,15,0),
+                height: 50,
+                child: ListView.builder(
+                    reverse: true,
+                    // shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: cats.length,
+                    itemBuilder: (BuildContext context, int index){
+                      return GestureDetector(
+                        onTap: (){setState(() {
+                          selectedListItem3=index;
+                        });},
+                        child: Container(
+                            padding: EdgeInsets.only(left: 40),
+                            child: Text(catListView[index], style: selectedListItem3==index?categoryListStyleSelected :categoryListStyle,)),
+                      );
+                    }
                 ),
-                Container(
-                    height:MediaQuery.of(context).orientation==Orientation.portrait? 500: 850,
-                    child: ProductList()),
-                Container(
-                    padding: EdgeInsets.all(20),
+              ),
+              FutureBuilder<List<dynamic>>(
+                future: ProductsRepository().getProducts(),
+                builder: (context, snapshot) {
 
-                    child: watchMoreButton((){},width)),
-                Container(
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
 
-                    child: imageText("assets/Banners/sale-banner.png",sDiscount,width,imageTextStyleWhite)),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0,20,15,0),
-                  height: 50,
-                  child: ListView.builder(
-                      reverse: true,
-                      // shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: cats.length,
-                      itemBuilder: (BuildContext context, int index){
-                        return GestureDetector(
-                          onTap: (){setState(() {
-                            selectedListItem3=index;
-                          });},
+                  } else if (snapshot.hasData) {
+                    return
+                      Container(
+                        height:MediaQuery.of(context).orientation==Orientation.portrait? 500:850,
+                        child: IgnorePointer(
                           child: Container(
-                              padding: EdgeInsets.only(left: 40),
-                              child: Text(catListView[index], style: selectedListItem3==index?categoryListStyleSelected :categoryListStyle,)),
-                        );
-                      }
-                  ),
-                ),
-                Container(
-                    height:MediaQuery.of(context).orientation==Orientation.portrait? 500: 850,
-                    child: ProductList()),
-                Container(
-                    padding: EdgeInsets.all(20),
+                            padding: EdgeInsets.all( 10),
+                            child: GridView.builder(
+                                gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  childAspectRatio:MediaQuery.of(context).orientation==Orientation.portrait? width/750: width/1300,
+                                ),
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return productShow(width / 3 - 10, snapshot.data[index]["media_url"],
+                                      price );
+                                }
+                            ),
+                          ),
+                        ),
+                      );
+                    /*Text(
+                      snapshot.data[0]["media_url"]
 
-                    child: watchMoreButton((){},width)),
+                    );*/
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+              Container(
+                  padding: EdgeInsets.all(20),
+
+                  child: watchMoreButton((){},width)),
 
 
 
@@ -226,11 +355,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
 
 
-              ],
-            ),
+
+            ],
           ),
         ),
-    ),
-      );
+      ),
+    );
   }
 }
